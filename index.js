69ccbccbc33e28f50d44613272357470fa4e4909
TO3IC <beded.noob@gmail.com>
2022-12-17 14:51:51 +0800

    added nav bar and media queries

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 var features = document.querySelector(".features");
 var featurebar = document.querySelector(".features-bar");
+var companybar = document.querySelector(".company-bar");
+var navlinks = document.querySelector(".navlinks");
+var hambuger = document.querySelector(".hamburger");
+var hambugerclose = document.querySelector(".hamburger-close");
+var company = document.querySelector(".company");
 var featureOpen = false;
+var companyOpen = false;
+var NavOpen = false;
 
 features.addEventListener("click", () => {
   if (featureOpen === false) {
     featureOpen = true;
     features.innerHTML = `Features<svg width="20" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 5 4-4 4 4"/></svg>`;
-    featurebar.classList.add("features-active");
+    featurebar.classList.add("active");
   } else {
     features.innerHTML = `Features <svg width="20" height="6" xmlns="http://www.w3.org/2000/svg">
           <path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4" />
         </svg>`;
-    featurebar.classList.remove("features-active");
-    featureOpen = false
+    featurebar.classList.remove("active");
+    featureOpen = false;
   }
 });
 
+company.addEventListener("click", () => {
+  if (companyOpen === false) {
+    companyOpen = true;
+    company.innerHTML = `Company<svg width="20" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 5 4-4 4 4"/></svg>`;
+    companybar.classList.add("active");
+  } else {
+    company.innerHTML = `Company<svg width="20" height="6" xmlns="http://www.w3.org/2000/svg"><path stroke="#686868" stroke-width="1.5" fill="none" d="m1 1 4 4 4-4" /></svg>`;
+    companybar.classList.remove("active");
+    companyOpen = false;
+  }
+});
 
+hambuger.addEventListener("click", () => {
+  navlinks.style.translate = "0 0";
+  hambuger.style.display = "none";
+});
+
+hambugerclose.addEventListener("click", () => {
+  navlinks.style.translate = "50vh 0";
+  hambuger.style.display = "flex";
+});
